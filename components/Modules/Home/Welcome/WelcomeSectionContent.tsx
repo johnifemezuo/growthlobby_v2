14f3c75e8f0e155202f79cfaccfe5218caa78e51
johnifemezuo <ifemezuojohn@gmail.com>
2025-04-23 13:23:42 +0100

    fixing build issue

--- components/Modules/Home/Welcome/WelcomeSectionContent.tsx
+++ components/Modules/Home/Welcome/WelcomeSectionContent.tsx
@@ -6,7 +6,7 @@ const WelcomeSectionContent = () => {
     <div className="text-[#808080] font-wixMadefor md:text-[28px] text-[20px] leading-normal font-semibold md:w-[40%] flex flex-col md:gap-6 gap-4 ">
           <TextReveal>Welcome to Growthlobby,where creativity meets strategy. We are a passionate collective of designers, developers, and visionaries, dedicated to transforming B2B digital innovation into unforgettable stories.</TextReveal>
           
-          <TextReveal>We don’t just create; we craft powerful brands, stunning websites, and cutting-edge apps that resonate, bridging the gap between vision and reality.</TextReveal>
+          <TextReveal>We don’t just create; we craft powerful brands, stunning websites, and cutting-edge apps that resonate, bridging the gap between vision and reality reality.</TextReveal>
           
           <TextReveal>Our collaborative approach ensures that every project is an enriching journey, making the process as rewarding as the ground-breaking results we deliver.</TextReveal>
           
